02d3a6d8ca71b9a8adb8e0c8a881b6e759f6e3a7
Gabegit config --global user.email escobedo.gabe@my.codergv.org <gabriel.escobedo01@utrgv.edu>
2016-10-27 05:06:06 -0500

    the view got the datafrom the database

--- contactlistapp/server.js
+++ contactlistapp/server.js
@@ -1,29 +1,17 @@
 var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('contactlist', ['contactlist']);
 
 app.use(express.static(__dirname + "/public"));
 
 app.get('/contactlist', function (req, res) {
   console.log("I received a GET request");
 
-  person1 = {
-    name: 'Tim',
-    email: 'tim@email1.com',
-    number: '(111) 111-1111'
-  };
-  person2 = {
-    name: 'Emily',
-    email: 'emily@email2.com',
-    number: '(222) 222-2222'
-  };
-  person3 = {
-    name: 'John',
-    email: 'john@email3.com',
-    number: '(333) 333-3333'
-  };
-
-  var contactlist = [person1, person2, person3];
-  res.json(contactlist);
+  db.contactlist.find(function (err,docs) {
+    console.log(docs);
+    res.json(docs);
+  });
 });
 
 /*
