4f5b9ef2705157680012fc5f372d958e519da67b
Gabegit config --global user.email escobedo.gabe@my.codergv.org <gabriel.escobedo01@utrgv.edu>
2016-10-27 03:23:53 -0500

    getting data from server

--- contactlistapp/public/controllers/controller.js
+++ contactlistapp/public/controllers/controller.js
@@ -2,24 +2,13 @@ var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
     console.log("Hello World from controller");
 
-    person1 = {
-      name: 'Tim',
-      email: 'tim@email1.com',
-      number: '(111) 111-1111'
-    };
-    person2 = {
-      name: 'Emily',
-      email: 'emily@email2.com',
-      number: '(222) 222-2222'
-    };
-    person3 = {
-      name: 'John',
-      email: 'john@email3.com',
-      number: '(333) 333-3333'
-    };
+$http.get('/contactlist').success(function(response) {
+  console.log("I got the data I requested");
+  $scope.contactlist = response;
+});
 
-    var contactlist = [person1, person2, person3];
-    $scope.contactlist = contactlist;
+
+    //$scope.contactlist = contactlist;
 
 
 }]);
--- contactlistapp/server.js
+++ contactlistapp/server.js
@@ -3,6 +3,29 @@ var app = express();
 
 app.use(express.static(__dirname + "/public"));
 
+app.get('/contactlist', function (req, res) {
+  console.log("I received a GET request");
+
+  person1 = {
+    name: 'Tim',
+    email: 'tim@email1.com',
+    number: '(111) 111-1111'
+  };
+  person2 = {
+    name: 'Emily',
+    email: 'emily@email2.com',
+    number: '(222) 222-2222'
+  };
+  person3 = {
+    name: 'John',
+    email: 'john@email3.com',
+    number: '(333) 333-3333'
+  };
+
+  var contactlist = [person1, person2, person3];
+  res.json(contactlist);
+});
+
 /*
 app.get('/', function (req, res) {
   res.send("Hello world from server.js");
